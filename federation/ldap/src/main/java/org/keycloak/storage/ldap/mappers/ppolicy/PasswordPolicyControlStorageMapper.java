886674de1f58cd88b678e2f8d6f96049718271cc
Daniele Gaffuri <d.gaffuri@reply.it>
2019-12-04 19:20:10 +0100

    Fix LDAP password policies for 8.0.1

--- federation/ldap/src/main/java/org/keycloak/storage/ldap/mappers/ppolicy/PasswordPolicyControlStorageMapper.java
+++ federation/ldap/src/main/java/org/keycloak/storage/ldap/mappers/ppolicy/PasswordPolicyControlStorageMapper.java
@@ -30,8 +30,8 @@ import org.jboss.logging.Logger;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.ModelException;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserCredentialModel;
 import org.keycloak.models.UserModel;
-import org.keycloak.models.credential.PasswordUserCredentialModel;
 import org.keycloak.storage.ldap.LDAPStorageProvider;
 import org.keycloak.storage.ldap.idm.model.LDAPObject;
 import org.keycloak.storage.ldap.idm.query.internal.LDAPQuery;
@@ -84,7 +84,7 @@ public class PasswordPolicyControlStorageMapper extends AbstractLDAPStorageMappe
     }
 
     @Override
-    public LDAPOperationDecorator beforePasswordUpdate(final UserModel user, final LDAPObject ldapUser, final PasswordUserCredentialModel password) {
+    public LDAPOperationDecorator beforePasswordUpdate(final UserModel user, final LDAPObject ldapUser, final UserCredentialModel password) {
         // Not apply policies if password is reset by admin (not by user himself)
         if (password.isAdminRequest()) {
             return null;
@@ -94,11 +94,11 @@ public class PasswordPolicyControlStorageMapper extends AbstractLDAPStorageMappe
     }
 
     @Override
-    public void passwordUpdated(final UserModel user, final LDAPObject ldapUser, final PasswordUserCredentialModel password) {
+    public void passwordUpdated(final UserModel user, final LDAPObject ldapUser, final UserCredentialModel password) {
     }
 
     @Override
-    public void passwordUpdateFailed(final UserModel user, final LDAPObject ldapUser, final PasswordUserCredentialModel password, final ModelException exception) {
+    public void passwordUpdateFailed(final UserModel user, final LDAPObject ldapUser, final UserCredentialModel password, final ModelException exception) {
         throw processFailedPasswordUpdateException(exception);
     }
 
